ffc972c72df19bd1965e4548ab9546338ccd33e9
pardoma <pardoma@uji.es>
2024-05-09 09:21:47 +0200

    feat: firebase class

---
+++ firebase.js
@@ -0,0 +1,50 @@
+export class FirebaseService {
+
+    URL_BASE = 'https://findinbok.firebaseio.com/';
+    bokMap = new Map();
+    versionsMap = new Map();
+    currentVersion;
+  
+    constructor() {
+      const currentVersionUrl = 'current/version.json';
+      this.getDataFromFirebase(currentVersionUrl).then((response) => {
+        this.currentVersion = 'v' + response;
+      });
+    }
+  
+    async getBokVersion(version) {
+      const queryVersion = version === this.currentVersion ? 'current' : version;
+      if (!this.bokMap.has(queryVersion)){
+        const bok = await this.getDataFromFirebase(queryVersion + '.json');
+        this.bokMap.set(queryVersion, bok);
+      }
+      return this.bokMap.get(queryVersion);
+    }
+  
+    async getOldVersionsData() {
+      if(this.versionsMap.size === 0) {
+        const versionsUrl = '.json?shallow=true';
+        const versionsObject = await this.getDataFromFirebase(versionsUrl);
+        delete versionsObject[this.currentVersion];
+        delete versionsObject['current'];
+        const versionsArray = Object.keys(versionsObject);
+        for(let key of versionsArray) {
+          const dateUrl = `${key}/creationYear.json`;
+          this.versionsMap.set(key, await this.getDataFromFirebase(dateUrl));
+        }
+      }
+      return this.versionsMap;
+    }
+  
+    async getDataFromFirebase(url) {
+      try {
+        const response = await fetch(this.URL_BASE + url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return await response.json();
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+    }
+  }
